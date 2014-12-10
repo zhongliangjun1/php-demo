3428b8be82b604a7c98d06ec4893bf219e4051e4
liangjun.zhong <zhongliangjun1@gmail.com>
2014-12-10 21:14:35 +0800

    array

--- array-demo.php
+++ array-demo.php
@@ -2,15 +2,31 @@
 
 	 <head>
 	 	<title>PHP ARRAY DEMO</title>
+	 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	 </head>
 	 <body>
-	 	<?php echo "<div>Your user agent is " . $_SERVER['HTTP_USER_AGENT'] . "</div>" ; ?> 
-
-	 	
+	
 	 	<?php 
+	 		echo "<h3>索引数组</h3>";
 	 		$myArray = array("hello","array","world");
 	 		echo "<div>" . $myArray[0] . " " . $myArray[1] . " " . $myArray[2] . " , my array count is " . count($myArray) . "</div>" ; 
 	 	?>
+
+	 	<?php 
+	 		echo "<h3>关联数组</h3>";
+	 		$myArray = array("word"=>"hello", "name"=>"zhong");
+	 		echo "<div>" . $myArray["word"] . " " . $myArray["name"] . " !</div>" ; 
+	 	?>
+
+	 	<?php
+	 		echo "<h3>遍历关联数组</h3>";
+			$age = array("Bill"=>"35","Steve"=>"37","Peter"=>"43");
+			foreach( $age as $x=>$x_value) {
+			  echo "key=" . $x . ", value=" . $x_value;
+			  echo "<br>";
+			}
+		?>
+
 
 	 </body>
 	 
